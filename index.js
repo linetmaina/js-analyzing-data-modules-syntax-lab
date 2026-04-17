e392a7d459b060e4670185cc39b3f1b1dad52da2
Linet Maina <linetnmaina@gmail.com>
2026-04-17 12:13:53 +0300

    Modules Lab

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+// First, I'm importing datejs
+require('datejs');
 
+// function to take many arrays of usernames & merge them
+function combineUsers(...args) {
+  // intializing object
+  const combinedObject = {
+    users: []
+  };
 
+  // Looping and merging arrays
+  for (let arr of args) {
+    combinedObject.users.push(...arr);
+  }
 
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+  // Adding current date
+  const today = new Date();
+  combinedObject.merge_date = today.toString("M/d/yyyy");
+
+  // Returning object
+  return combinedObject;
+}
+
+// exporting the function for test
+
+module.exports = { combineUsers };
